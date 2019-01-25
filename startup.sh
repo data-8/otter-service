fe372d33bc7e937efb3a62f72e83d5f7bff97d0a
Vincent Su <vipasu@berkeley.edu>
2019-01-25 19:39:04 +0000

    Update startup script to ensure that grading virtualenv is sourced

--- startup.sh
+++ startup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Using the technology of systemd to keep gofer service running even after system restarts
 # place a corresponding unit service file in your systemd unit path (/etc/systemd/system/ for most)
+source ~/gradingenv/bin/activate
 python gofer_nb.py
