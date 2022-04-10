276ce8719a830de0d2fe003028c5839127faa7e3
Sean Morris <sean.smorris@berkeley.edu>
2022-04-10 16:31:54 -0700

    Deployment by namespace by git branch
    The deployment now deploys to a namespace that uses the git
    branch name: prod, staging, or dev

--- deployment/cloud/deploy-nfs.sh
+++ deployment/cloud/deploy-nfs.sh
@@ -1,13 +1,33 @@
 Reference: https://medium.com/@ngoodger_7766/nfs-filestore-on-gcp-for-free-859593e18bdf
 
-gcloud compute addresses create gofer-nfs-private-ip \
+gcloud compute addresses create gofer-nfs-prod-private-ip \
     --addresses 10.128.0.31 \
     --region us-central1 \
     --subnet default
 
-gcloud compute instances create gofer-nfs-server --private-network-ip=10.128.0.31 --boot-disk-size=30GB \
+gcloud compute addresses create gofer-nfs-staging-private-ip \
+    --addresses 10.128.0.34 \
+    --region us-central1 \
+    --subnet default
+
+gcloud compute addresses create gofer-nfs-dev-private-ip \
+    --addresses 10.128.0.33 \
+    --region us-central1 \
+    --subnet default
+
+gcloud compute instances create gofer-nfs-prod --private-network-ip=10.128.0.31 --boot-disk-size=30GB \
  --image=ubuntu-2004-focal-v20220308 --image-project=ubuntu-os-cloud --machine-type=f1-micro --tags=nfs
-gcloud compute ssh nfs-server --zone=us-central1-c
+
+gcloud compute instances create gofer-nfs-staging --private-network-ip=10.128.0.34 --boot-disk-size=10GB \
+ --image=ubuntu-2004-focal-v20220308 --image-project=ubuntu-os-cloud --machine-type=f1-micro --tags=nfs
+
+gcloud compute instances create gofer-nfs-dev --private-network-ip=10.128.0.33 --boot-disk-size=10GB \
+ --image=ubuntu-2004-focal-v20220308 --image-project=ubuntu-os-cloud --machine-type=f1-micro --tags=nfs
+
+gcloud compute ssh gofer-nfs-prod --zone=us-central1-c
+gcloud compute ssh gofer-nfs-staging --zone=us-central1-c
+gcloud compute ssh gofer-nfs-dev --zone=us-central1-c
+
 sudo apt install -y nfs-kernel-server
 sudo mkdir /data
 sudo chown nobody:nogroup /data
@@ -15,6 +35,8 @@ sudo chmod 777 /data
 sudo vim /etc/exports
 /data *(rw,sync,no_subtree_check)
 sudo systemctl restart nfs-kernel-server
+
+
 gcloud compute firewall-rules create nfs --allow=tcp:111,udp:111,tcp:2049,udp:2049 --target-tags=nfs
 
 
--- deployment/cloud/deploy.sh
+++ deployment/cloud/deploy.sh
@@ -1,19 +1,40 @@
-version=$(<src/gofer_service/__init__.py)
-version=${version##__version__ = }
-version=`sed -e 's/^"//' -e 's/"$//' <<<"$version"`
-python3 -m twine upload dist/*$version*
+# version=$(<src/gofer_service/__init__.py)
+# version=${version##__version__ = }
+# version=`sed -e 's/^"//' -e 's/"$//' <<<"$version"`
+# python3 -m twine upload dist/*$version*
 
-github_key=$(sops -d src/gofer_service/secrets/gh_key.yaml)
-github_key=${github_key##github_access_token: }
-gcloud builds submit --substitutions=_GITHUB_KEY=$github_key,_TAG_NAME=$version  --config ./deployment/cloud/cloudbuild.yaml
+# github_key=$(sops -d src/gofer_service/secrets/gh_key.yaml)
+# github_key=${github_key##github_access_token: }
+# gcloud builds submit --substitutions=_GITHUB_KEY=$github_key,_TAG_NAME=$version  --config ./deployment/cloud/cloudbuild.yaml
 
-./deployment/cloud/deploy-service-account.sh
-kubectl apply -f ./deployment/cloud/deployment-persistent-volume.yaml
-kubectl apply -f ./deployment/cloud/deployment-persistent-volume-claim.yaml
-# we ignore the checksum so that clear text values can be changes for deployments -- like POST_GRADE can can be made false
-# for testing and more
-sops -d --ignore-mac ./deployment/cloud/deployment-config-encrypted.yaml | kubectl apply -f -
-kubectl apply -f ./deployment/cloud/deployment.yaml
-kubectl apply -f ./deployment/cloud/deployment-service.yaml
+branch_name=$(git symbolic-ref -q HEAD)
+branch_name=${branch_name##refs/heads/}
+branch_name=${branch_name:-HEAD}
+export KUBECONFIG=./kube-context
+gcloud container clusters get-credentials grader-cluster --region us-central1 --project data8x-scratch
+kubectl config use "gke_data8x-scratch_us-central1_grader-cluster"
+if [ "$branch_name" == "staging" -o "$branch_name" == "prod" -o "$branch_name" == "dev" ]; then
+    NFS_IP=$(gcloud compute addresses list --filter="name=( 'gofer-nfs-$branch_name-private-ip')" --format="get(address)" 2>&1)
+    NAMESPACE=grader-$branch_name
+    kubectl config set-context --current --namespace=$NAMESPACE
+    kubectl create namespace $NAMESPACE --save-config --dry-run=client -o yaml | kubectl apply -f -
+    ./deployment/cloud/deploy-service-account.sh
+    
+    NFS_IP=$(gcloud compute addresses list --filter="name=( 'gofer-nfs-$branch_name-private-ip')" --format="get(address)")
+    yq eval ".spec.nfs.server=\"$NFS_IP\"" -i deployment/cloud/deployment-persistent-volume.yaml
+    yq eval ".metadata.name=\"gofer-volume-$branch_name\"" -i deployment/cloud/deployment-persistent-volume.yaml
+    kubectl apply -f ./deployment/cloud/deployment-persistent-volume.yaml 
+    kubectl apply -f ./deployment/cloud/deployment-persistent-volume-claim.yaml
+    
+    # we ignore the checksum so that clear text values can be changes for deployments -- like POST_GRADE can can be made false
+    # for testing and more
+    sops -d --ignore-mac ./deployment/cloud/deployment-config-encrypted.yaml | kubectl apply -f -
+    kubectl apply -f ./deployment/cloud/deployment.yaml
+    kubectl apply -f ./deployment/cloud/deployment-service.yaml
 
-./deployment/cloud/gcp-workload-identity.sh
+    kubectl apply -f ./deployment/cloud/deployment-autoscale-vertical-pod-rec.yaml
+    kubectl apply -f ./deployment/cloud/deployment-autoscale-horizontal-pod.yaml
+
+    ./deployment/cloud/gcp-workload-identity.sh
+fi
+rm -f ./kube-context
