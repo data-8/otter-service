4027390461273a95c14d974b081a6c251081367e
Sean Morris <sean.smorris@berkeley.edu>
2022-05-06 15:31:02 -0700

    Rename all resources to gofer-*
    Some resources were named grader-*; everything is now gofer-*

--- deployment/cloud/deploy-service-account.sh
+++ deployment/cloud/deploy-service-account.sh
@@ -2,38 +2,38 @@
 NAMESPACE=`kubectl config view --minify --output 'jsonpath={..namespace}'`
 
 SA_KUBE=$(kubectl get serviceaccounts 2>&1)
-if [[ ! $SA_KUBE =~ "grader-k8-sa" ]]; then
-    kubectl create serviceaccount --namespace $NAMESPACE grader-k8-sa
+if [[ ! $SA_KUBE =~ "gofer-k8-sa" ]]; then
+    kubectl create serviceaccount --namespace $NAMESPACE gofer-k8-sa
 fi
 
 #GCP config:
-SA_OUTPUT=$(gcloud iam service-accounts describe grader-sa@data8x-scratch.iam.gserviceaccount.com 2>&1)
+SA_OUTPUT=$(gcloud iam service-accounts describe gofer-sa@data8x-scratch.iam.gserviceaccount.com 2>&1)
 if [[ $SA_OUTPUT =~ "Unknown service account" ]] || [[ $SA_OUTPUT =~ "PERMISSION_DENIED" ]]; then
-    gcloud iam service-accounts create grader-sa
+    gcloud iam service-accounts create gofer-sa
 fi
 
 # #Add all the roles we need:
 gcloud projects add-iam-policy-binding data8x-scratch \
-    --member "serviceAccount:grader-sa@data8x-scratch.iam.gserviceaccount.com" \
+    --member "serviceAccount:gofer-sa@data8x-scratch.iam.gserviceaccount.com" \
     --role "roles/editor" \
     --no-user-output-enabled
 
 gcloud projects add-iam-policy-binding -q data8x-scratch \
-    --member "serviceAccount:grader-sa@data8x-scratch.iam.gserviceaccount.com" \
+    --member "serviceAccount:gofer-sa@data8x-scratch.iam.gserviceaccount.com" \
     --role "roles/cloudkms.cryptoKeyEncrypterDecrypter" \
     --no-user-output-enabled
 
 
 #Tie the K8 SA to GCloud SA:
 #Allow impersonation:
-gcloud iam service-accounts add-iam-policy-binding -q grader-sa@data8x-scratch.iam.gserviceaccount.com \
+gcloud iam service-accounts add-iam-policy-binding -q gofer-sa@data8x-scratch.iam.gserviceaccount.com \
     --role roles/iam.workloadIdentityUser \
-    --member "serviceAccount:data8x-scratch.svc.id.goog[$NAMESPACE/grader-k8-sa]" \
+    --member "serviceAccount:data8x-scratch.svc.id.goog[$NAMESPACE/gofer-k8-sa]" \
     --no-user-output-enabled
 
 
 #Annotate the K8 Service account:
 kubectl annotate serviceaccount \
-        --namespace $NAMESPACE grader-k8-sa \
+        --namespace $NAMESPACE gofer-k8-sa \
         --overwrite \
-        iam.gke.io/gcp-service-account=grader-sa@data8x-scratch.iam.gserviceaccount.com
+        iam.gke.io/gcp-service-account=gofer-sa@data8x-scratch.iam.gserviceaccount.com
--- deployment/cloud/deploy.sh
+++ deployment/cloud/deploy.sh
@@ -11,32 +11,42 @@ branch_name=$(git symbolic-ref -q HEAD)
 branch_name=${branch_name##refs/heads/}
 branch_name=${branch_name:-HEAD}
 export KUBECONFIG=./kube-context
-gcloud container clusters get-credentials grader-cluster --region us-central1 --project data8x-scratch
-kubectl config use "gke_data8x-scratch_us-central1_grader-cluster"
+gcloud container clusters get-credentials gofer-cluster --region us-central1 --project data8x-scratch
+kubectl config use "gke_data8x-scratch_us-central1_gofer-cluster"
 if [ "$branch_name" == "staging" -o "$branch_name" == "prod" -o "$branch_name" == "dev" ]; then
     NFS_IP=$(gcloud compute addresses list --filter="name=( 'gofer-nfs-$branch_name-private-ip')" --format="get(address)" 2>&1)
-    LB_IP=$(gcloud compute addresses list --filter="name=( 'grader-v2-external-ip-$branch_name')" --format="get(address)" 2>&1)
-    NAMESPACE=grader-$branch_name
+    LB_IP=$(gcloud compute addresses list --filter="name=( 'gofer-v2-external-ip-$branch_name')" --format="get(address)" 2>&1)
+    NAMESPACE=gofer-$branch_name
     kubectl config set-context --current --namespace=$NAMESPACE
     kubectl create namespace $NAMESPACE --save-config --dry-run=client -o yaml | kubectl apply -f -
     ./deployment/cloud/deploy-service-account.sh
     
-    yq eval ".spec.nfs.server=\"$NFS_IP\"" -i deployment/cloud/deployment-persistent-volume.yaml
-    yq eval ".metadata.name=\"gofer-volume-$branch_name\"" -i deployment/cloud/deployment-persistent-volume.yaml
-    kubectl apply -f ./deployment/cloud/deployment-persistent-volume.yaml 
-    kubectl apply -f ./deployment/cloud/deployment-persistent-volume-claim.yaml
+    #yq eval ".spec.nfs.server=\"$NFS_IP\"" -i deployment/cloud/deployment-nfs-volume.yaml
+    yq eval ".metadata.name=\"gofer-nfs-volume-$branch_name\"" -i deployment/cloud/deployment-nfs-volume.yaml
+    
+    kubectl apply -f ./deployment/cloud/deployment-nfs-volume.yaml 
+    kubectl apply -f ./deployment/cloud/deployment-nfs-volume-claim.yaml
+
+    kubectl apply -f ./deployment/cloud/deployment-nfs-gke.yaml
+    kubectl apply -f ./deployment/cloud/deployment-nfs-service.yaml 
+
+    # yq eval ".spec.nfs.server=\"$NFS_IP\"" -i deployment/cloud/deployment-persistent-volume.yaml
+    # yq eval ".metadata.name=\"gofer-volume-$branch_name\"" -i deployment/cloud/deployment-persistent-volume.yaml
+    
+    # kubectl apply -f ./deployment/cloud/deployment-persistent-volume.yaml 
+    # kubectl apply -f ./deployment/cloud/deployment-persistent-volume-claim.yaml
     
     # we ignore the checksum so that clear text values can be changes for deployments -- like POST_GRADE can can be made false
     # for testing and more
-    sops -d --ignore-mac ./deployment/cloud/deployment-config-encrypted.yaml | kubectl apply -f -
-    kubectl apply -f ./deployment/cloud/deployment.yaml
+    # sops -d --ignore-mac ./deployment/cloud/deployment-config-encrypted.yaml | kubectl apply -f -
+    # kubectl apply -f ./deployment/cloud/deployment.yaml
     
-    yq eval ".spec.loadBalancerIP=\"$LB_IP\"" -i deployment/cloud/deployment-service.yaml
-    kubectl apply -f ./deployment/cloud/deployment-service.yaml
+    # yq eval ".spec.loadBalancerIP=\"$LB_IP\"" -i deployment/cloud/deployment-service.yaml
+    # kubectl apply -f ./deployment/cloud/deployment-service.yaml
 
-    kubectl apply -f ./deployment/cloud/deployment-autoscale-vertical-pod-rec.yaml
-    kubectl apply -f ./deployment/cloud/deployment-autoscale-horizontal-pod.yaml
+    # kubectl apply -f ./deployment/cloud/deployment-autoscale-vertical-pod-rec.yaml
+    # kubectl apply -f ./deployment/cloud/deployment-autoscale-horizontal-pod.yaml
 
-    ./deployment/cloud/gcp-workload-identity.sh
+    # ./deployment/cloud/gcp-workload-identity.sh
 fi
 rm -f ./kube-context
