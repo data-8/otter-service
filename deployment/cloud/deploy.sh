013ba008988408908c9f71555d93f51eb0c64f43
Sean Morris <sean.smorris@berkeley.edu>
2022-06-14 18:48:56 -0700

    Worked out JUPYTERHUB_SERVICE_PREFIX
    I needed to include the Jupyterhub Service Prefix(/services/...) in order
    for the proxy to work correctly

--- deployment/cloud/deploy.sh
+++ deployment/cloud/deploy.sh
@@ -1,18 +1,18 @@
 version=$(<src/otter_service/__init__.py)
 version=${version##__version__ = }
 version=`sed -e 's/^"//' -e 's/"$//' <<<"$version"`
-# python3 -m twine upload dist/*$version*
-
-# github_key=$(sops -d src/otter_service/secrets/gh_key.yaml)
-# github_key=${github_key##github_access_token: }
+#python3 -m twine upload dist/*$version*
+# gcloud container --project "data8x-scratch" clusters create-auto "otter-cluster" --region "us-central1" --release-channel "regular" --network "projects/data8x-scratch/global/networks/default" --subnetwork "projects/data8x-scratch/regions/us-central1/subnetworks/default"
+github_key=$(sops -d src/otter_service/secrets/gh_key.yaml)
+github_key=${github_key##github_access_token: }
+# if breaks on Permission denied run: gcloud auth login
 # gcloud builds submit --substitutions=_GITHUB_KEY=$github_key,_TAG_NAME=$version  --config ./deployment/cloud/cloudbuild.yaml
 
-# gcloud container --project "data8x-scratch" clusters create-auto "otter-cluster" --region "us-central1" --release-channel "regular" --network "projects/data8x-scratch/global/networks/default" --subnetwork "projects/data8x-scratch/regions/us-central1/subnetworks/default"
 branch_name=$(git symbolic-ref -q HEAD)
 branch_name=${branch_name##refs/heads/}
 branch_name=${branch_name:-HEAD}
 export KUBECONFIG=./kube-context
-gcloud container clusters get-credentials otter-cluster --region us-central1 --project data8x-scratch
+gcloud container clusters get-credentials otter-cluster --region us-central1 --project data8x-scratch                  
 kubectl config use "gke_data8x-scratch_us-central1_otter-cluster"
 if [ "$branch_name" == "staging" -o "$branch_name" == "prod" -o "$branch_name" == "dev" ]; then
     NFS_IP=$(gcloud compute addresses list --filter="name=( 'otter-nfs-$branch_name-private-ip')" --format="get(address)" 2>&1)
