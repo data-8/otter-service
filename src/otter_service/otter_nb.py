d1575716ffb3ad9d30035394ce62d965e3c1b46f
Sean Morris <sean.smorris@berkeley.edu>
2022-07-13 15:39:08 -0700

    Added Signal handling to stop server

--- src/otter_service/otter_nb.py
+++ src/otter_service/otter_nb.py
@@ -2,6 +2,8 @@ import base64
 import json
 import time
 import logging
+import signal
+from functools import partial
 import traceback
 from datetime import datetime
 import os
@@ -439,7 +441,11 @@ def start_server():
     access_file_handler.setFormatter(formatter)
     access_logger.addHandler(access_file_handler)
 
-    app.listen(10101)
+    server = tornado.httpserver.HTTPServer(app)
+    server.listen(10101)
+
+    signal.signal(signal.SIGTERM, partial(sig_handler, server))
+    signal.signal(signal.SIGINT, partial(sig_handler, server))
 
     return app
 
@@ -461,6 +467,30 @@ def main():
         tornado.ioloop.IOLoop.current().start()
     except Exception:
         logging.getLogger('tornado.application').error(traceback.format_exc())
+
+
+def sig_handler(server, sig, frame):
+    io_loop = tornado.ioloop.IOLoop.instance()
+    MAX_WAIT_SECONDS_BEFORE_SHUTDOWN = 3
+
+    def stop_loop(deadline):
+        now = time.time()
+        if now < deadline and (io_loop._callbacks or io_loop._timeouts):
+            logging.info('Waiting for next tick')
+            io_loop.add_timeout(now + 1, stop_loop, deadline)
+        else:
+            io_loop.stop()
+            logging.info('Shutdown finally')
+
+    def shutdown():
+        logging.info('Stopping http server')
+        server.stop()
+        logging.info('Will shutdown in %s seconds ...',
+                     MAX_WAIT_SECONDS_BEFORE_SHUTDOWN)
+        stop_loop(time.time() + MAX_WAIT_SECONDS_BEFORE_SHUTDOWN)
+
+    logging.warning('Caught signal: %s', sig)
+    io_loop.add_callback_from_signal(shutdown)
 
 
 if __name__ == '__main__':
