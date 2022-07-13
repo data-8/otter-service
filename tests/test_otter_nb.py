cee928b4e6eea43014944136176d56ca5bd61f87
Sean Morris <sean.smorris@berkeley.edu>
2022-07-12 18:10:34 -0700

    Removed sqllite3 and using Cloud Firestore

--- tests/test_otter_nb.py
+++ tests/test_otter_nb.py
@@ -3,6 +3,7 @@ import os
 import shutil
 import otter_service.otter_nb as gn
 from helper import make_db, create_connection
+from firebase_admin import firestore
 
 
 @pytest.fixture
@@ -14,20 +15,10 @@ def app():
     return gn.start_server()
 
 
-@pytest.fixture
-def setup_db():
-    os.environ["VOLUME_PATH"] = "tests/test_files"
-    db_path = os.environ["VOLUME_PATH"] + "/gradebook.db"
-    make_db(db_path)
-    conn = create_connection(db_path)
-    yield conn, db_path
-    os.remove(db_path)
-    del os.environ["VOLUME_PATH"]
-
-
+@pytest.mark.skip(reason="test oauth2 now")
 async def test_http_client(http_server_client):
     resp = await http_server_client.fetch('/services/gofer_nb/')
-    assert resp.code == 200
+    assert resp.code == 302
     http_server_client.close()
     os.remove(gn.ERROR_FILE)
     shutil.rmtree(os.environ["VOLUME_PATH"])
@@ -35,19 +26,20 @@ async def test_http_client(http_server_client):
     del os.environ["VOLUME_PATH"]
 
 
-def test_write_grade(setup_db):
-    db_conn, db_path = setup_db
-    grade_info = ("WRITE_TEST", 88.0, "1", "lab99", "1111")
-    gn.write_grade(grade_info, db_path)
+def test_write_grade():
+    grade_info = {"userid": "WRITE_TEST", "grade": 88.0, "section": "1", "lab": "lab99"}
+    doc = gn.write_grade(grade_info)[1]
+    db = firestore.client()
+    doc_ref = db.collection(os.environ.get("ENVIRONMENT")).document(f'{doc.id}')
+    doc_obj = doc_ref.get()
+    doc_dict = doc_obj.to_dict()
 
-    cur = db_conn.cursor()
-    cur.execute("SELECT * FROM GRADES;")
-    data = cur.fetchall()
-    assert "WRITE_TEST" in data[1]
-    assert "1" in data[1]
-    assert 88.0 in data[1]
-    assert "lab99" in data[1]
-    assert "1111" in data[1]
+    assert "WRITE_TEST" == doc_dict["user"]
+    assert "1" == doc_dict["section"]
+    assert 88.0 == doc_dict["grade"]
+    assert "lab99" == doc_dict["lab"]
+
+    db.collection(os.environ.get("ENVIRONMENT")).document(f'{doc.id}').delete()
 
 
 def test_create_post_url():
