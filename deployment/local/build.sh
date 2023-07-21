f9d6a18f059dc44edfddcff9f8cc5f712148a63c
sean-morris <sean.smorris@gmail.com>
2023-07-21 09:21:10 -0700

    s

--- deployment/local/build.sh
+++ deployment/local/build.sh
@@ -1,3 +1,4 @@
+lsof -t -i:8080 -i:4401 -i:4501 -i:4400 -i:9000 -i:9099 -i:9199 -i:9090 | xargs kill -9
 version=$(<src/otter_service/__init__.py)
 version=${version##__version__ = }
 version=`sed -e 's/^"//' -e 's/"$//' <<<"$version"`
